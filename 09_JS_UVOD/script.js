5adb2505edb0f1159e354edf47dc4ef4cc884857
DesertFox9449 <aleksandarzikicnumb@gmail.com>
2021-11-27 20:10:47 +0100

    Kraj druge nedelje, uvod u JS, vezbanje CSS-a

---
+++ 09_JS_UVOD/script.js
@@ -0,0 +1,140 @@
+console.log("Pozdrav iz JavaScript datoteke!");
+
+// let x = 3;
+// console.log(x);
+
+// javlja se greska jer je let x deklarisana
+// u drugom JS fajlu
+// sve datoteke interaguju jedna sa drugom u HTML-u
+// Postoje moduli koji razdvajaju kod datoteka
+// radicemo to kasnije 
+
+let a = 3, b = 6;
+// isto sto i
+// let a = 3;
+// let b = 6;
+console.log(a * b);
+
+console.log ("Pera" + " " + "Stankovic")
+//isto sto i
+console.log ("Pera" + " Stankovic")
+console.log ("Pera" * " Stankovic") // NaN (Not a Number)
+
+b = b / a;
+console.log (a, b); // a = 3. b = 2
+a = b * a + a; // 2 * 3 + 3 = 6 + 3 = 9
+console.log(a, b); // a = 9, b = 2
+
+b = (a - 2 * b) / b; // (9 - 2 * 2) / 2 = (9 - 4) / 2 = 5 / 2 = 2.5
+console.log(a, b);
+
+
+
+
+//a = a + 10; // 9 + 10 = 19 
+// promenljiva a se povecava za 10
+// isto sto i
+a += 10;
+console.log(a);
+
+// b = b / a; je isto sto i
+b /= a;
+console.log(b);
+
+// ++ povecava vrednost za 1
+// -- smanjuje vrednost za 1
+
+// a = a + 1;
+// a += 1;
+//a++; // 3 razlicita nacina 
+// a = 19 + 1 / a = 20
+
+// moze i  
+++a
+console.log(a);
+
+// a++ i ++a su nekad iste vrednosti, nekad ne
+
+let c = 5;
+console.log(c++); 
+// Prvo se iskoristi stara vrednost promenljive u izrazu, pa se onda vrednost promenjive poveca / c = 5
+console.log(c); 
+// Tek nakon narednog pozivanja se vrednost povecava za 1/ c = 5 + 1 = 6
+
+let d = 3;
+console.log(++d);
+// Vrednost promenjive je povecana, pa se onda nova vrednost koristi u izrazu / d = 3 + 1 = 4
+console.log(d);
+// Vrednost je takodje 4
+
+c = 4;
+d = 3; 
+x = (c++) * (--d); // 4 * 2 = 8
+console.log(c, d, x); 
+// c = ? d = ? x = ?
+// c = 5 d = 2 x = 8
+
+//Deljenje po modulu/ OStatak pri deljenju
+console.log(7 % 2); // 7 = 3 * 2 + 1 ====1
+console.log(7 % 2); // 9 = 4 * 2 + 1 ====1
+console.log(180 % 60); // 180 = 3 * 60 + 0 ====0 
+// Modul sa 60 moguci rezultati 0, 1, 2, ..., 59
+
+console.log(365 % 7);
+// razlika u danima u godini 1
+
+console.log(4 ** 3);
+// Stepenovanje
+
+c = 3;
+d = 1 + (c ** 3) // 1 + (3 * 3 * 3) = 1 + 27 = 28
+
+// Nisu svuda potrebne zagrade
+// Prati se matematicka prednost operacija
+
+// Zadatak 1
+var sati = 10;
+var minuti = 27;
+var prosliSati =  sati;
+var prosliMinuti =  minuti;
+
+console.log("Proslo je tacno " + sati + " sati" + " i " + minuti + " minuta od ponoci!");
+
+// Zadatak 2
+var sviMinuti = sati * 60 + minuti;
+
+console.log("Proslo je tacno " + sviMinuti + " minuta od ponoci!")
+
+// Zadatak 3
+var cenaRobe = 1250;
+var uplata = 2000;
+var kusur = uplata - cenaRobe;
+console.log("Vas kusur je " + kusur + " dinara!") 
+
+// Zadatak 4
+var dinar = 2000;
+var evro = 200;
+const razlika = 117
+
+var dinarVsEuro = dinar / razlika;
+console.log("Za " + dinar + " dinara mozete kupiti " + dinarVsEuro + " evra");
+
+var euroVsDinar = evro * razlika;
+console.log("Za " + evro + " evra mozete kupiti " + euroVsDinar + " dinara");
+
+//Zadatak 5
+
+var dolar = 20;
+var evro2 = 50;
+const razlika2 = 1.1;
+
+var dolarVsEuro = dolar / razlika2;
+console.log("Za " + dolar + " dolara mozete dobiti " + dolarVsEuro + " evra");
+
+var evroVsDolar = evro2 * razlika2;
+console.log("Za " + evro2 + " evra mozete dobiti " + evroVsDolar + " dolara");
+
+
+
+
+
---
+++ 09_JS_UVOD/vezbanja.js
@@ -0,0 +1,162 @@
+//"use strict"; // Strict - mora da se kuca sve bez greske, ide u prvoj liniji koda
+
+/* 
+// Zadatak 1
+var sati = 10;
+var minuti = 27;
+var prosliSati =  sati;
+var prosliMinuti =  minuti;
+
+console.log("Proslo je tacno " + sati + " sati" + " i " + minuti + " minuta od ponoci!");
+
+// Zadatak 2
+var sviMinuti = sati * 60 + minuti;
+
+console.log("Proslo je tacno " + sviMinuti + " minuta od ponoci!")
+
+// Zadatak 3
+var cenaRobe = 1250;
+var uplata = 2000;
+var kusur = uplata - cenaRobe;
+console.log("Vas kusur je " + kusur + " dinara!") 
+
+// Zadatak 4
+var dinar = 2000;
+var evro = 200;
+const razlika = 117
+
+var dinarVsEuro = dinar / razlika;
+console.log("Za " + dinar + " dinara mozete kupiti " + dinarVsEuro + " evra");
+
+var euroVsDinar = evro * razlika;
+console.log("Za " + evro + " evra mozete kupiti " + euroVsDinar + " dinara");
+
+//Zadatak 5
+
+var dolar = 20;
+var evro2 = 50;
+const razlika2 = 1.1;
+
+var dolarVsEuro = dolar / razlika2;
+console.log("Za " + dolar + " dolara mozete dobiti " + dolarVsEuro + " evra");
+
+var evroVsDolar = evro2 * razlika2;
+console.log("Za " + evro2 + " evra mozete dobiti " + evroVsDolar + " dolara");
+
+*/
+
+// Zdatak 1
+let s = 15;
+let m = 21;
+const danMin = 24 * 60; 
+
+let = odPonoci = s * 60 + m;
+console.log("Proslo je " + odPonoci + " minuta od ponoci!");
+
+let = doPonoci = danMin - odPonoci; // (24 - s) * 60 - m;
+
+// Zadatak 2
+
+// Prvi nacin
+let minOdPonoci = 921; 
+// minOdPonoci = _____ * 50 + minSada
+// minOdPonoci - minSada = ____ * 60
+// (minOdPonoci - minSada) / 60 = ____
+let minSada = minOdPonoci % 60;
+let satSada = (minOdPonoci - minSada) /60;
+console.log("Sada je " + satSada + ":" + minSada);
+
+// Drugi nacin
+//console.log(minOdPonoci / 60); // Deljenje varaca realan broj
+//console.log(Math.floor(minOdPonoci / 60)); //- ceo broj manji ili jednak od rezultata 
+// Treba nam Math.floor
+
+//console.log(Math.ceil(minOdPonoci / 60)); // - ceo broj manji ili jednak od rezultata 
+//console.log(Math.round(minOdPonoci / 60)); // - ceo broj rezultat zaokruzivanja
+
+let satSada2 = Math.floor(minOdPonoci / 60);
+let minSada2 = minOdPonoci % 60;
+console.log("Sada je " + satSada2 + ":" + minSada2);
+
+//Greska prilikom deklarisanja
+// k = 9;
+// console.log(k);
+// automatski je deklarise kao     var, sto moze da bude problem ako je ti ne deklarises
+
+// Zadatak 3
+
+let cenaRobe = 1750;
+let novcanica = 2000;
+
+let kusur = novcanica - cenaRobe;
+console.log("Kusur koji treba da se vrati je " + kusur + " dinara!");
+
+// Zadatak 6
+
+
+// let juro = 100;
+// let din = 2000;
+// const kursEd = 117; 
+
+// let juroVsDin = juro * kursEd;
+// console.log("Mozete kupiti " + juroVsDin + " dinara za " + juro + " evra!");
+
+// let dinVsJuro = din / kursEd;
+// console.log("Mozete kupiti " + dinVsJuro + " evra za " + din + " dinara!");
+
+// // Zadatak 7
+
+// let euro = 100;
+// let dollar = 200;
+// let dinar = 5000;
+// const kursDd = 104;
+// let dollarVsDinar = dollar * kursDd;
+// console.log("Mozete kupiti " + dollarVsDinar + " dinara za " + dollar + " dolara!");
+
+// let dinarVsDollar = dinar / kursDd;
+// console.log("Mozete kupiti " + dinarVsDollar + " dolara za " + dinar + " dinara!");
+
+// const kursDolaraUEvre = 0.88;
+// let dollarVsEvro = dollar * kursDolaraUEvre;
+// console.log("Mozete kupiti " + dollarVsEvro + " evra za " + dollar + " dolara!");
+
+// let evroVsDollar = euro / 0.88;
+// console.log("Mozete kupiti " + evroVsDollar + " dolara za " + euro + " evra!");
+
+// Zadatak 6 Stefan
+
+let novacEvri = 200;
+let kursEvri = 1117.5782;
+
+let evriUDinare = novacEvri * kursEvri;
+console.log("imamo ");
+
+let novacDinari = 1000;
+let dinariUEvre = novacDinari / kursEvri;
+console.log("Imamo ");
+
+// Zadatak 7 Stefan
+
+let kursDolari = 104.7281
+
+let k1 = novacEvri * kursEvri;
+let k2 = k1 / kursDolari;
+console.log("Imamo " + k2 + " dolara nakon razmere");
+
+let novacDolari = 200;
+let k3 = novacDolari * kursDolari / kursEvri;
+console.log("Imamo " + k3 + " dolara nakon razmere" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
