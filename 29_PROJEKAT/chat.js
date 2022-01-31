249f105d6bf400d00bd4c08f29e05bd49ce6bf43
DesertFox9449 <aleksandarzikicnumb@gmail.com>
2022-01-31 14:38:25 +0100

    TASK_2 Konekcija ka Firestore bazi podataka

---
+++ 29_PROJEKAT/chat.js
@@ -0,0 +1,9 @@
+export class Chatroom {
+    constructor(r, uName, ch){
+        this.r = room;
+        this.uName = username;
+        this.ch = chats;
+    }
+    
+    
+}
---
+++ 29_PROJEKAT/script.js
@@ -0,0 +1,6 @@
+/* Kada kreiramo novi razgovor, želimo da znamo:
+U kojoj sobi se kreira razgovor
+Koja osoba kreira razgovor
+Kako izgleda trenutna kolekcija u bazi podataka na koju razgovor treba dodati
+*/
+
