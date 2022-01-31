1023867a86d3fa2a43c605ced23c95b7d6ea4cd0
DesertFox9449 <aleksandarzikicnumb@gmail.com>
2022-01-31 22:14:10 +0100

    Task 4, ulepsavanje

--- 29_PROJEKAT/chat.js
+++ 29_PROJEKAT/chat.js
@@ -15,30 +15,66 @@ export class Chatroom {
     }
     // za uName
     set username(uName) {
-        this._username = uName;
+        uName = uName.trim();
+        if(uName.length >= 2 && uName.length <= 10){
+            this._username = uName;
+        }
+        else{
+            alert(`Korsinicko ime treba da sadrzi izmedju 2 i 10 karaktera.`)
+        }
+        
     }
     get username() {
         return this._username;
     }
     
-    
-    // addChat metoda
-    // addChat() {
-    //     db.collection(`chats`)
-    //     .add({
-    //         message: , //value text inputa,
-    //         username: this.username,
-    //         room: // value
 
-    //     })
-    //     .this(() => {
-    //         console.log(`Objekat je dodat u bazu podataka`);
-    //     })
-    //     .catch((err) => {
-    //         console.log(`Doslo je do greske prilikom dodavanja objekta: ${err}`);
-    //     });
-    // }
-    
+    ////////////////////// Jelena
+    // dodavanje nove poruke
+    async addChat(mess){
+        // dodavanje trenutnog vremena
+        let date = new Date();
+
+        // Kreiranje dokumenta/obj prosledjujem bazi podataka
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room:   this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(date)
+        }
+
+        // Da sacuvam dokument u db
+        // let response = await this.chats.add(`Uspesno dodat cet`)
+        // return response; // vracam Promise() i mogu za njega da kazem .then() .catch
+        // // dodavanje dokumenta u bazu podataka
+        // this.chats
+
+        
+        // Da sačuvan dokument u db
+        let response = await this.chats.add(docChat);
+        return response; // Vraćam Promise i mogu za njega da kažem .then i .catch 
+    }
+
+    // Metod koji prati promene u bazi i vraca poruke
+    getChats(callback) {
+        this.chats.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+
+                // Kada se desila promena u bazi
+                // console.log(change.type);
+                // if(change.type == `added`){
+                //     console.log("Promena u bazi");
+                // }
+
+                // Ispisati sve dokumente koji su dodati u bazu
+                if(change.type == `added`) {
+                    // console.log(change.doc.data());
+                    callback(change.doc.data());
+                }
+                
+            });
+        })
+    }
 }
 
 
--- 29_PROJEKAT/script.js
+++ 29_PROJEKAT/script.js
@@ -1,11 +1,12 @@
 import { Chatroom } from "./chat.js";
-// export default db;
+
 
 let chat1 = new Chatroom(`JS`, `Stefan`);
 console.log(chat1);
 
-let chat2 = new Chatroom(`general`, `Jelena`, db);
-let chat3 = new Chatroom(`tests`, `Miljan`, db);
+let chat2 = new Chatroom(`general`, `Jelena`);
+let chat3 = new Chatroom(`tests`, `Miljan`);
+
 
 let chatroom1 = new Chatroom(`js`, `Pera`);
 console.log(chatroom1);
@@ -22,3 +23,13 @@ console.log(chatroom1.username);
 // room
 chatroom1.room = `general`;
 console.log(chatroom1.room);
+
+
+
+// chat3.addChat(`Imamo HR trening u sredu!`)
+// .then(() => console.log(`Sve je proslo kako treba.`))
+// .catch((err) => console.log(`Doslo je do greske. ${err}`));
+
+// chatroom1.getChats((ocekivani_argument) => {
+//     console.log(ocekivani_argument);
+// })
