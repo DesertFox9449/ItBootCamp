655b79735a75295866ca366acfd774a8c956ca9f
DesertFox9449 <aleksandarzikicnumb@gmail.com>
2022-02-01 14:40:56 +0100

    Task_7 Dodavanje ui.js fajla Dodavanje ChatUI klase

---
+++ 29_PROJEKAT/ui.js
@@ -0,0 +1,17 @@
+export class ChatUI {
+    constructor(cr) {
+        this.chatRoom = cr;
+    }
+    set lista(cr) {
+        this._chatRoom = cr
+    }
+    get lista() {
+        return this._chatRoom;
+    }
+    
+    templateLI () {
+
+    }
+
+
+}
